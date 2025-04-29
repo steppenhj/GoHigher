f1efdf8292130baa0acb5d2579f23461061da0f5
steppenhj <phj1675@naver.com>
2025-04-29 19:39:41 +0900

    알림도 추가

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = 'gohigher-v2';
+const CACHE_NAME = 'gohigher-v3';
 // ⚠️ 이 배열에 들어 있는 URL은 “실제 브라우저가 요청하는 경로”와 1:1 매칭되어야 합니다!
 const urlsToCache = [
   '/',                    // start_url
@@ -142,3 +142,25 @@ async function fetchLatestData() {
     console.error('❌ Periodic Sync failed:', error);
   }
 }
+
+importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCgFLtAo8LETpHq44hxlT7QigCbIltk-Zk",
+  authDomain: "gohigher-55e51.firebaseapp.com",
+  projectId: "gohigher-55e51",
+  storageBucket: "gohigher-55e51.firebasestorage.app",
+  messagingSenderId: "487435343721",
+  appId: "1:487435343721:web:dc5708c3a263214fba4ff8"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log("📥 백그라운드 메시지 수신:", payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon-192x192.png"
+  });
+});
